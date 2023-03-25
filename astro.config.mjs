731221475c54a1483bdc35cffa0efe488221733d
Franklin Shera <fshera96@gmail.com>
2023-03-25 04:21:31 +0300

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import image from "@astrojs/image";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), image()]
+  integrations: [tailwind(), image(), compress()],
+  output: "server",
+  adapter: node({
+    mode: "middleware",
+  }),
 });
