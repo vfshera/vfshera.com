a6f5de8e3a7030d4e89d30f60b52a5ed99901906
Franklin Shera <fshera96@gmail.com>
2023-06-18 16:23:22 +0300

    disable x-powered

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import { handler as ssrHandler } from "./dist/server/entry.mjs";
 
 const app = express();
+app.disable("x-powered-by");
+
 app.use(express.static("dist/client/"));
 app.use(ssrHandler);
 
