7224c4cdc83f703e2b3b925e4bff58ddca9a5f2c
Franklin Shera <fshera96@gmail.com>
2023-07-15 22:19:18 +0300

    add my data

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,4 +4,48 @@ export const APP = {
   logoText: "vfshera",
 } as const;
 
-export const socials = [];
+export const tagLine =
+  "Based in Kenya, I'm a front-end developer passionate about building accessible web apps that users love.";
+
+export const socials = [
+  {
+    name: "Github",
+    link: "https://github.com/vfshera",
+  },
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/vfshera",
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/1vfshera",
+  },
+] as const;
+
+export const skills = [
+  {
+    title: "React",
+    startYear: 2021,
+  },
+  {
+    title: "Vue",
+    startYear: 2020,
+  },
+  {
+    title: "Astro",
+    startYear: 2022,
+  },
+
+  {
+    title: "Node",
+    startYear: 2021,
+  },
+  {
+    title: "Php",
+    startYear: 2019,
+  },
+  {
+    title: "Laravel",
+    startYear: 2019,
+  },
+];
