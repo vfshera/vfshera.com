99c3ff565e02e5b3992a62d0498bd8d369be7284
Franklin Shera <fshera96@gmail.com>
2023-07-17 00:52:58 +0300

    sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import image from "@astrojs/image";
 
 import node from "@astrojs/node";
- 
+
 export default defineConfig({
-  integrations: [tailwind(), image(), compress()],
+  experimental: {
+    assets: true,
+  },
+  integrations: [tailwind(), compress()],
   output: "server",
+  image: {
+    service: sharpImageService(),
+  },
   adapter: node({
     mode: "middleware",
   }),
