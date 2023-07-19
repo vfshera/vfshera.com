bde30de0dfc084a5ada83b70b02dd46e9681c119
Franklin Shera <fshera96@gmail.com>
2023-07-19 19:27:49 +0300

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { defineMiddleware } from "astro/middleware";
+import { APP } from "./data";
 
-export const onRequest = defineMiddleware(({ request, url }, next) => {
-  //   if (url.pathname.startsWith("/api")) {
-  //     console.log("API REQ");
-  //     console.log(request);
-  //     console.log(url);
-  //   }
+export const onRequest = defineMiddleware(({ url }, next) => {
+  if (url.pathname.startsWith("/api")) {
+    if (url.origin != APP.DOMAIN) {
+      return new Response(JSON.stringify({}), { status: 200 });
+    }
+  }
 
   return next();
 });
