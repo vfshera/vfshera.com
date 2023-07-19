9219b7ea22a0459b651d0009e0b980bfc7825597
Franklin Shera <fshera96@gmail.com>
2023-07-19 17:43:05 +0300

    alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-
 import node from "@astrojs/node";
+import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), alpinejs()],
   output: "server",
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
   adapter: node({
-    mode: "middleware",
-  }),
+    mode: "middleware"
+  })
 });
