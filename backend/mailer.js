aff65c91f2f312e9a55c5f7102e320718b8998a5
Franklin Shera <fshera96@gmail.com>
2023-07-21 18:51:15 +0300

    mailer

---
+++ backend/mailer.js
@@ -0,0 +1,11 @@
+import { createTransport } from "nodemailer";
+
+export const transporter = createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
