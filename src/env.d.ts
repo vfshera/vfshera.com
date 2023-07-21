c56fb23d2adec2ce82a2859302df9f1c3dd67537
Franklin Shera <fshera96@gmail.com>
2023-07-21 18:52:11 +0300

    env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="astro/client-image" />
+
+interface ImportMetaEnv {
+  readonly DBHOST: string;
+  readonly DBUSER: string;
+  readonly DBPASS: string;
+  readonly DATABASE: string;
+  readonly DOMAIN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
