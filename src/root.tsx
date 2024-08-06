6208b647bb56676de6ad9fd75bb3645e2346eec0
Franklin Shera <fshera96@gmail.com>
2024-08-06 04:35:19 +0300

    add analytics

--- src/root.tsx
+++ src/root.tsx
@@ -7,12 +7,14 @@ import {
 
 import "~/global.css";
 import Head from "~/components/common/Head";
+import { QwikPartytown } from "~/components/partytown";
 
 export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
+        <QwikPartytown forward={["_paq"]} />
         <link rel="manifest" href="/manifest.json" />
         <Head />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -24,6 +26,25 @@ export default component$(() => {
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
+        />
+
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={`
+           var _paq = window._paq = window._paq || []; 
+          _paq.push(['trackPageView']);
+          _paq.push(['disableCookies']);          
+          _paq.push(['enableLinkTracking']);
+          _paq.push(['enableHeartBeatTimer']);
+          _paq.push(['trackVisibleContentImpressions', true, 500]);
+          (function() {
+            var u="//a.vfshera.com/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+          `}
         />
         <ServiceWorkerRegister />
       </head>
