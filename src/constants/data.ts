2cef5a691c2eca17e8711279c0a5453e526bfe4d
Franklin Shera <fshera96@gmail.com>
2025-01-21 16:49:24 +0300

    update projects

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -29,6 +29,39 @@ export const skills = [
     startYear: 2019,
   },
 ];
+export const ossProjects: {
+  title: string;
+  description: string;
+  tags: string[];
+  repo: string;
+  link?: string;
+  wip?: boolean;
+}[] = [
+  {
+    title: "Shuriken UI - React",
+    description:
+      "Shuriken UI is a free and open-source Tailwind CSS UI Kit. It is a collection of components and templates that you can use to build your next Tailwind CSS project.",
+    tags: ["UI Components", "React", "Tailwindcss", "Typescript"],
+    repo: "https://github.com/shuriken-ui/react",
+    link: "https://shurikenui.com/",
+  },
+  {
+    title: "Fastify Directory Routes",
+    description:
+      "A Fastify plugin that enables file system-based routing, utilizing directories to specify URL segments matched by the router.",
+    tags: ["Fastify Plugin", "Typescript"],
+    repo: "https://github.com/vfshera/fastify-dir-routes",
+    link: "https://www.npmjs.com/package/@vfshera/fastify-dir-routes",
+  },
+  {
+    title: "Dep",
+    description:
+      "Your CPANEL Deployment Tool - Designed to simplify the process of deploying CPANEL sites.",
+    tags: ["Qwik", "Tailwindcss", "Typescript", "Drizzle ORM"],
+    repo: "https://github.com/vfshera/dep",
+    wip: true,
+  },
+];
 
 export const projects = [
   {
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,10 +1,10 @@
-import { projects } from "~/constants/data";
+import { ossProjects, projects } from "~/constants/data";
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
     <section title="Projects" class="projects">
-      <div class="wrapper py-[140px]">
+      <div class="wrapper py-[140px] space-y-20">
         <div class="grid grid-cols-2 gap-10 justify-center text-center sm:justify-start sm:text-left sm:gap-y-[60px] sm:gap-x-6 lg:gap-y-[70px] lg:gap-x-[30px]">
           <h2 class="col-span-2 sm:col-span-1 text-[clamp(2.2rem,0.5rem+6.5vw,4.5rem)] leading-[1.2] -tracking-[.028em]">
             Projects
@@ -50,6 +50,71 @@ export default component$(() => {
               </div>
             </div>
           ))}
+        </div>
+
+        <div>
+          <h2 class="text-[clamp(1.6rem,0.5rem+3.5vw,3.5rem)] leading-[1.2] -tracking-[.028em]">
+            OSS
+          </h2>
+          <p class="text-xl">Open source projects I've contributed to.</p>
+
+          <div class="space-y-6 mt-5">
+            {ossProjects.map((project, index) => (
+              <div
+                key={index}
+                class="px-4 py-3 border  border-white/40 rounded-lg"
+              >
+                <p class="text-xl font-bold mb-1 text-[var(--accent)]">
+                  {project.title}
+                  {project.wip && <span class="opacity-70"> (🚧 WIP)</span>}
+                </p>
+                <p class="mb-2 text-lg max-w-[90%]">{project.description}</p>
+                <div class="flex justify-between items-center mt-3">
+                  <div class="flex gap-2">
+                    {[...new Set(project.tags)].map((tag) => (
+                      <p class="" key={tag}>
+                        <span class="text-[var(--accent)]">#</span>
+                        {tag}
+                      </p>
+                    ))}
+                  </div>
+                  <div class="flex gap-5 mt-3 *:flex *:gap-1.5">
+                    <a
+                      href={project.repo}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="25"
+                        height="24"
+                        aria-labelledby="socialGitHub"
+                        role="img"
+                      >
+                        <path
+                          fill="#FFF"
+                          fill-rule="evenodd"
+                          d="M12.304 0C5.506 0 0 5.506 0 12.304c0 5.444 3.522 10.042 8.413 11.672.615.108.845-.261.845-.584 0-.292-.015-1.261-.015-2.291-3.091.569-3.891-.754-4.137-1.446-.138-.354-.738-1.446-1.261-1.738-.43-.23-1.046-.8-.016-.815.97-.015 1.661.892 1.892 1.261 1.107 1.86 2.876 1.338 3.584 1.015.107-.8.43-1.338.784-1.646-2.738-.307-5.598-1.368-5.598-6.074 0-1.338.477-2.446 1.26-3.307-.122-.308-.553-1.569.124-3.26 0 0 1.03-.323 3.383 1.26.985-.276 2.03-.415 3.076-.415 1.046 0 2.092.139 3.076.416 2.353-1.6 3.384-1.261 3.384-1.261.676 1.691.246 2.952.123 3.26.784.861 1.26 1.953 1.26 3.307 0 4.721-2.875 5.767-5.613 6.074.446.385.83 1.123.83 2.277 0 1.645-.015 2.968-.015 3.383 0 .323.231.708.846.584a12.324 12.324 0 0 0 8.382-11.672C24.607 5.506 19.101 0 12.304 0Z"
+                        />
+                      </svg>
+                      Repository
+                    </a>
+                    {project.link && (
+                      <a
+                        href={project.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Link
+                      </a>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+
+            <p>... and so much more!</p>
+          </div>
         </div>
       </div>
     </section>
